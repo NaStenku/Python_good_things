b9145a549505363bc178f9d09901e05ec7fd5d72
NaStenku <mastenku1@gmail.com>
2020-02-21 12:32:44 +0200

    Second version

--- Monty_Hall_problem.py
+++ Monty_Hall_problem.py
@@ -1,22 +1,11 @@
-from random import shuffle
-doors = {1: "goat", 2: "goat", 3: "car"}
+import random
 choice = int(input("Choose the door number(1,2 or 3):"))
-# def shuffle_new():
-#     keys = list(doors.keys())
-#     shuffle(keys)
-#     Shuffled_doors = dict()
-#     for key in keys:
-#         Shuffled_doors.update({doors[key]: key})
-#     return Shuffled_doors
-op = 1
-if choice == 1:
-    op = 2
-else:
-    op = 1
-print ("Ok. But i want to open one more door for you. Here is door", op , "there is a goat. Do you still want to open the door number", choice, "?")
-choice2 = int(input("Choose the door number(1,2 or 3):"))
-if doors[choice2] == "car":
-    print("Congratulation!")
-else:
-    print("Sorry, maybe next time")
+doors = [1,2,3]
+car = random.randint (1,3)
+open_door = list(filter(lambda x: x != car and x != choice, doors))[0]
+close_door = list(filter(lambda x: x != choice and x != open_door, doors))[0]
+print ("Ok. But I want to open one door for you. Now I will open the door number", open_door, "and we will see what's there. Oh! Goat here! Do you still want to open the door number", choice, "?")
+choice2 = int(input("Choose the door number: %s or %s ?" % (choice, close_door)))
+print("Congratulation!" if choice2 == car else  "Maybe next time")
+
 
